38eab755eef01f565e9dc07eb56bfa0dfb7227f0
Peter Knipp <peteraknipp@gmail.com>
2024-11-20 18:29:16 -0500

    Again try simplest pattern (ie w/out Option).

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -12,7 +12,7 @@ class Application @Inject()(val controllerComponents: ControllerComponents, val 
     Ok(views.html.index())
   }
 
-  def showPath(path: Option[String]) = Action {
+  def showPath(path: String) = Action {
     Ok(views.html.results(path))
   }
 
